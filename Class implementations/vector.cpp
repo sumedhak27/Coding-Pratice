9747b0d10443e27cb3fa9c74eb562f20a41c1c30
Sumedh Kulkarni <sumedh.a.kulkarni@seagate.com>
2022-09-26 21:09:53 +0530

    added parital vector implementation.
    Signed-off-by: Sumedh Kulkarni <sumedh.a.kul27@gmail.com>

---
+++ Class implementations/vector.cpp
@@ -0,0 +1,71 @@
+// https://godbolt.org/#z:OYLghAFBqd5TKALEBjA9gEwKYFFMCWALugE4A0BIEAZgQDbYB2AhgLbYgDkAjF%2BTXRMiAZVQtGIHgBYBQogFUAztgAKAD24AGfgCsp5eiyahUAUgBMAIUtXyKxqiIEh1ZpgDC6egFc2TEAtydwAZAiZsADk/ACNsUhAANgBOcgAHdCViFyYvX39A9MzsoTCI6LY4hJT7bEdnIREiFlIiPL8Anlr6nKaWojKo2PiDJWbW9oKusf7BiqqpAEp7dB9SVE4uSwBmcNRfHABqM22PFzHSbHYT3DMtAEEdvYPsY9PsdQ20hqYbu8eLLsmPsfEcTh4OGwyABPP4Pf5EbBsNJGRFvDxEaFpZjsV4AFTh932LCUSkOADVsE4yMcAOw2B6HJmHMaYEAgJRIFrYTAAfW%2BpHBBIArDZhQARG6HFqkFiw7YM%2B7MlkEABe2F5REO4jSLFQxHliuVWXVmpV6pOiv%2ByoAbugCJhDphVjFGLydXqDRBFnSjcrmR79ZjDgAqE7iw4WS3W/1M1nszncvkCoVmUVpyXbXCHCIAdxlcreEfjIDYLAA1hrE5dMKn0xKbhBAwbFtHGbHDoJSIcIOEtQQi4ctJbDgPwebsCOCLZbD6zPSYx3mXmC7DRdOJYPV2mrBvM37Y/PJe3Y0p82kICvSLLoeRpde5a2FYu6cfHu20j5XQRUCAX5TqVIXthEOd0WDSQceB9EBtXAz1MQgMC0mWCcIC0OcFxPf1V0HEsy0rXlqx5OsxUzXAmzgoNoSfA9X3%2BF8AHoGIpKkSCAgC2NbLCOLICAS3CbIJDVeJeXoAgxlTLNDjEsZoNg3UqIgGSiAAOhNbBvRQ9S0Iw2jlRw8MWSINlSwrKsuRrEiM0bZtMRol9lS7HsWB8EhLESQ5OGk8SiC4pUl2ZbdRXUmdrAzQdJ2fLCj3orC7QdQ5P05XkYj1csIDxTzhFIajfQc5kiByvKsKXAgaB7dS3lubYI1s6j8oCp0XTdOrvTbfzGqCqwQrCsLN0M5hCsNBr/RGo9YKIVAkB7DAmDGIyTI%2BL4fnczzdJG41jPZDBXPRcdLAsXBrzIGCDr205PJU3MuSIb1zo8Bb2Xceh2qXGLotpN8XxoUgCHcR6QEyQqrjYVb0GxWU2PBcE%2BK2wGLhBsHXLvWb5p4wVTgJLNVptKl1pK5knOA/tB2HBVR3RQ5cdQNThKnUKrD8xqmVWLVoYu6mVK6vd7uOCwLD5qMoo6/1Wd5g60w8X5%2BdejtLiINYmEOVnZaZGLhZfAA/dG7vnGxPvog2NYePtDjLcJdcwkX0fBPspRtHhVcOJiWMA23hHtiwIAd%2BysJdm3TjtqSbW2QcHadgOzg94OBcMvWeC0cggm2bZyGkdXaKJ02xxqocp0phP6d6xnipF20eBUpKkBStLe19kX3ut1iyHBEsLnCYB7e2OOF35pR0A4IgkA7g67wOuoVAlo3aJLHa2dOccQ95ktnqd/3m%2B7G0vb15O0/IYUj3rl9Z7F9mHvD4XNpMufea3tfmPRqnQ8Mi/aPlxW841z6uGWehuGFfgAQuA6HIOgbgHgGYslWOsV4Ow%2BDkCINoH%2ByxyxJGFCpZItIeD822FoCwPBhQAA4LC0lTn/Lg0hAFINAdwfgSgQCJ0QcAn%2B5A4CwBQBgZEDB4iUGoJwtI3CEjAGwUEOg9BESkHoRAGI1CYjhBaNCbg8DOEcGEAAeSYPQRRzDyA4DLCYSQOjCCXCcAQXG9CdEfCpK5TYIC%2Bx1GoWJGIsocpeBwNQwqBA2BKJYTQIwwAlDkj%2BrmNR2IgHwMEMIMQEhOAyDkMIZQahNA6P0F0IwJg0AM0MAQGI9DIDLHBj8CxABaVk4ZzDFwsCAhwrEchuCYJ4bwHQDChHCEMSoIwugZCyD8SYnQig9JyHMYYCRph1FqY0cYbQmlTG6BMpgfRWjDI6aM%2BwUy%2BmjCmcshYUEVhrA2AYYGmw%2BC/3/lQnRYCuDqEIYkYpiRpCHGAKgVAhxsEqQFhACBxc7z4GIDSOBd4vBcMYN2OBix%2BBMJ0IsFBIBtjSBUngwhtJaQpFpIQ6QPBaQWESIYbglDyBAJAZcuhDCEFIOhbirgFh%2BDeMIdsFSwpEiEK0LSYUPBMXbFpFoQhhCCXUOJWS5hyw2HIEBq5T8RBeFNgHgIkFkRcTcGubc%2B5jznmvIsO8/gPI/m/TZF0SJohxCSDiQaxJGhqGpPILmWUaQfGnK4AAvlFzuBqPFbtdA5UlV3IeU8l5byPlAtlfEPm2woIQvJcsJAVwcAJG9JS/F3ieCJBUtIUNyQLDSB5diiwyR2VOqJbQ%2BwpLIXIMpdsc5BauDhqFcsXGki6nSCAA
+
+#include <iostream>
+#include <exception>
+#include <memory>
+
+template <typename T>
+class Vector {
+    std::shared_ptr<T[]> array;
+    size_t capacity;
+    size_t size;
+
+    void double_capacity() {
+        capacity *= 2;
+        std::shared_ptr<T[]> newarray = std::make_shared<T[]>(capacity);
+        for (int i = 0; i < size; i++) {
+            newarray[i] = array[i];
+        }
+        swap(newarray, array);
+    }
+
+  public:
+    Vector(int _cap = 1) : capacity(_cap), size(0) {
+        array = std::make_shared<T[]>(capacity);
+    }
+
+    // Vector(Vector)
+    Vector(std::initializer_list<T> list) : capacity(list.size()), size(0) {
+        array = std::make_shared<T[]>(capacity);
+        for (auto& e: list)
+            array[size++] = e;
+    }
+
+    void push_back(T entry) {
+        try {
+            if (size >= capacity)
+                double_capacity();
+            array[size++] = entry;
+            
+        } catch (const std::exception& e) {
+            std::cout << "Error: " << e.what() << std::endl;
+        }
+    }
+
+    friend std::ostream& operator<<(std::ostream& out, const Vector<T>& vec) {
+        for (int i = 0; i < vec.size; i++)
+            out << vec.array[i] << " ";
+        out << "\n";
+        return out;
+    };
+
+    ~Vector() {}
+};
+
+int main() {
+    Vector<int> v1;
+    // Vector<int> v2(v1);
+    // Vector<int> v3 = v1;
+    Vector<int> v2 = {10,2,33,4};
+    for (int i = 0; i < 10; i++) {
+        v1.push_back(i);
+    }
+    Vector<std::string> v3= {"something", "else"};
+    std::cout << v3 << std::endl;
+    // Vector v2({2,3,4,5});
+
+    std::cout << v1;
+    std::cout << v2;
+    // Vector v3 = v1;
+    return 0;
+}
